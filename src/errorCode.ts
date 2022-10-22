9f8a626700fc810f103fc03f46fe80a051813736
changfeng <changfeng@gaoding.com>
2022-10-22 23:13:03 +0800

    chore: check jsx path exists before execute it

---
+++ src/errorCode.ts
@@ -0,0 +1,4 @@
+export const ErrorCode = {
+    JSX_PATH_NOT_EXISTS: 1,
+    APPLICATION_NOT_FOUND: 2,
+};
--- src/jsx.ts
+++ src/jsx.ts
@@ -5,6 +5,7 @@ import consola from 'consola';
 
 import { PhotoshopExecutor } from './executors/photoshop';
 import { exec, pathExists } from './utils.js';
+import { ErrorCode } from './errorCode';
 
 // create folder to storage all scripts related to this package
 const storageDir = path.resolve(os.homedir(), '.adobe_jsx');
@@ -13,11 +14,16 @@ if (!(await pathExists(storageDir))) {
 }
 
 const jsxPath = path.resolve(process.cwd(), process.argv[2]);
+if (!(await pathExists(jsxPath))) {
+    consola.error(`The path of jsx script doesn't exists: ${jsxPath}`);
+    process.exit(ErrorCode.JSX_PATH_NOT_EXISTS);
+}
+
 const psExecutor = new PhotoshopExecutor();
 const ps = await psExecutor.findApp();
 if (!ps) {
     consola.error(`Can't find the adobe application to execute jsx file: ${jsxPath}`);
-    process.exit(1);
+    process.exit(ErrorCode.APPLICATION_NOT_FOUND);
 }
 
 const jsxContent = await fs.readFile(jsxPath, { encoding: 'utf-8' });
